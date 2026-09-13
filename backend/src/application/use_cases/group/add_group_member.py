0a5a0ad73c464bfcfc294d1da327320651cda254
jnvillamor <jnvillamor@careerteam.com>
2026-09-13 23:35:54 +0800

    backend(feat): publish domain events from add group member use case.

--- backend/src/application/use_cases/group/add_group_member.py
+++ backend/src/application/use_cases/group/add_group_member.py
@@ -5,6 +5,7 @@ from dataclasses import dataclass
 from application.dto.group_dto import GroupView
 from application.exceptions import NotFoundError
 from domain.entities.group import Group, GroupMembershipError
+from domain.events.publisher import EventPublisher
 from domain.repositories.group_repository import GroupRepository
 from domain.value_objects.ids import GroupId, UserId
 
@@ -14,8 +15,9 @@ class AddGroupMemberInput:
   user_id: str
 
 class AddGroupMemberUseCase:
-  def __init__(self, group_repository: GroupRepository):
+  def __init__(self, group_repository: GroupRepository, event_publisher: EventPublisher):
     self._groups = group_repository
+    self._publisher = event_publisher
 
   def execute(self, input_data: AddGroupMemberInput) -> GroupView:
     group_id = GroupId(input_data.group_id)
@@ -27,4 +29,5 @@ class AddGroupMemberUseCase:
 
     group.add_member(user_id)
     self._groups.save(group)
+    self._publisher.publish(group.pull_events())
     return GroupView.from_entity(group)
