0d89662d2716490fe4d56084cb4ea98f096efb18
jnvillamor <jnvillamor@careerteam.com>
2026-09-14 14:40:05 +0800

    backend(feat): publish domain events from create group use case.

--- backend/src/application/use_cases/group/create_group.py
+++ backend/src/application/use_cases/group/create_group.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from dataclasses import dataclass
 
 from application.dto.group_dto import GroupView
+from domain.events.publisher import EventPublisher
 from domain.entities.group import Group
 from domain.repositories.group_repository import GroupRepository
 from domain.value_objects.ids import GroupId, UserId
@@ -13,10 +14,12 @@ class CreateGroupInput:
   created_by: str
 
 class CreateGroupUseCase:
-  def __init__(self, group_repository: GroupRepository):
+  def __init__(self, group_repository: GroupRepository, event_publisher: EventPublisher):
     self._groups = group_repository
+    self._publisher = event_publisher
 
   def execute(self, input_data: CreateGroupInput) -> GroupView:
     group = Group.create(id=GroupId.new(), name=input_data.name, created_by=UserId(input_data.created_by))
     self._groups.save(group)
+    self._publisher.publish(group.pull_events())
     return GroupView.from_entity(group)
